f13e056232e684de0c6dcb8ca29640367302b621
Rafa Adamy <95262339+AdamyMonteiro@users.noreply.github.com>
2022-07-12 01:42:10 -0300

    duration config

--- main.js
+++ main.js
@@ -34,28 +34,28 @@ sr.reveal('.whatsicon', {
 sr.reveal('.linkedIcon', {
   origin: 'bottom',
   distance: '400px', // A distancia do origin para a posição original do elemento.
-  duration: 1100, // A duração da animação em ms
-  delay: 400, // O delay para a animação acontecer
+  duration: 1000, // A duração da animação em ms
+  delay: 300, // O delay para a animação acontecer
   reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
 })
 sr.reveal('.instaicon', {
   origin: 'bottom',
   distance: '400px', // A distancia do origin para a posição original do elemento.
-  duration: 1200, // A duração da animação em ms
+  duration: 1000, // A duração da animação em ms
   delay: 400, // O delay para a animação acontecer
   reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
 })
 sr.reveal('.faceicon', {
   origin: 'bottom',
   distance: '400px', // A distancia do origin para a posição original do elemento.
-  duration: 1300, // A duração da animação em ms
+  duration: 1000, // A duração da animação em ms
   delay: 500, // O delay para a animação acontecer
   reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
 })
 sr.reveal('.giticon', {
   origin: 'bottom',
   distance: '400px', // A distancia do origin para a posição original do elemento.
-  duration: 1400, // A duração da animação em ms
+  duration: 1000, // A duração da animação em ms
   delay: 600, // O delay para a animação acontecer
   reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
 })
